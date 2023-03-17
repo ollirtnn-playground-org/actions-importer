13f5fc7d2d75c381b78033205ccb4c43c70015e3
Olli Rautiainen <74360204+ollirtnn@users.noreply.github.com>
2023-03-17 13:16:11 +0000

    default runner to transformers

--- transformers.rb
+++ transformers.rb
@@ -12,7 +12,7 @@ env "isMain", "${{ github.ref == 'refs/heads/main' }}"
 # Convert runners
 # first parameter is the name of the runner in Azure Pipelines
 # second parameter is the updated value of the runner to be used in GitHub Actions
-runner "mechamachine", "ubuntu-latest"
+runner :default , "my-runnergroup"
 
 # Convert DotnetCoreCLI@2 tasks to support parameters that are glob patterns and run the command for each project
 # glob patterns are like *.csproj or **/*.csproj
